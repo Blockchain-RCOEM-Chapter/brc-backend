59baadeac6a428db9091c47c578d9be68bd8b70f
Sohel Sheikh <sheikhsohel1007@gmail.com>
2025-03-02 12:31:28 +0530

    Added Crud For Events

--- schema/Event.model.js
+++ schema/Event.model.js
@@ -4,7 +4,7 @@ const EventSchema = new mongoose.Schema({
     EventBrochure: { type: String, required: true },
     Name: { type: String, required: true },
     Description: { type: String, required: true },
-    Date: { type: String, required: true },
+    Date: { type: Date, required: true },
     Time: { type: String, required: true },
     Venue: { type: String, required: true },
 });
